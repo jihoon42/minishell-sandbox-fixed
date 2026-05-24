e709ed63abf3689c3934236b475677d50719d8a2
JiHoon Kim <137034546+jihoon42@users.noreply.github.com>
2026-05-25 07:45:35 +0900

    Improve error message for invalid identifier in export

--- builtin_export_unset.c
+++ builtin_export_unset.c
@@ -34,7 +34,9 @@ static int	export_one(char *arg, t_shell *sh)
 
 	if (!is_valid_identifier(arg))
 	{
-		ft_putstr_fd("minishell: export: not a valid identifier\n", 2);
+		ft_putstr_fd("minishell: export: `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
 		return (1);
 	}
 	eq = ft_strchr(arg, '=');
